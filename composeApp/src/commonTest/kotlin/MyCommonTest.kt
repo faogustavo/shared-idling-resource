5b9a854d14cd02e0c230db203b97fb5a39aeb0b5
Gustavo Fao Valvassori <faogustavo@gmail.com>
2024-11-11 17:48:12 -0300

    updated to kotlin 2.0.21 and compose 1.7.0

--- composeApp/src/commonTest/kotlin/MyCommonTest.kt
+++ composeApp/src/commonTest/kotlin/MyCommonTest.kt
@@ -1,36 +1,72 @@
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.waitUntilAtLeastOneExists
 import kotlin.test.Test
 
 @OptIn(ExperimentalTestApi::class)
 class MyCommonTest {
+    @Test
+    fun myTest_idling() =
+        runComposeUiTest {
+            registerIdlingResource(MyCustomCommonIdlingResource)
+
+            setContent { MyScreen() }
+
+            onNodeWithTag("ValuePresenter")
+                .assertExists()
+                .assertTextEquals("0")
+
+            onNodeWithTag("IncrementButton")
+                .assertExists()
+                .performClick()
+
+            onNodeWithTag("ValuePresenter")
+                .assertExists()
+                .assertTextEquals("1")
+
+            onNodeWithTag("DecrementButton")
+                .assertExists()
+                .performClick()
+
+            onNodeWithTag("ValuePresenter")
+                .assertExists()
+                .assertTextEquals("0")
+
+            unregisterIdlingResource(MyCustomCommonIdlingResource)
+        }
 
     @Test
-    fun myTest() = runComposeUiTest {
-        registerIdlingResource(MyCustomCommonIdlingResource)
-        waitForIdle()
+    fun myTest_composeTestOnly() =
+        runComposeUiTest {
+            setContent { MyScreen() }
 
-        setContent { MyScreen() }
+            onNodeWithTag("ValuePresenter")
+                .assertExists()
+                .assertTextEquals("0")
 
-        onNodeWithTag("ValuePresenter")
-            .assertExists()
-            .assertTextEquals("0")
+            onNodeWithTag("IncrementButton")
+                .assertExists()
+                .performClick()
 
-        onNodeWithTag("IncrementButton")
-            .assertExists()
-            .performClick()
+//            onNodeWithTag("ValuePresenter")
+//                .assertExists()
+//                .assertTextEquals("1")
 
-        onNodeWithTag("ValuePresenter")
-            .assertExists()
-            .assertTextEquals("1")
+            waitUntilAtLeastOneExists(hasTestTag("ValuePresenter") and hasText("1"))
 
-        onNodeWithTag("DecrementButton")
-            .assertExists()
-            .performClick()
+            onNodeWithTag("DecrementButton")
+                .assertExists()
+                .performClick()
 
-        onNodeWithTag("ValuePresenter")
-            .assertExists()
-            .assertTextEquals("0")
+//            onNodeWithTag("ValuePresenter")
+//                .assertExists()
+//                .assertTextEquals("0")
 
-        unregisterIdlingResource(MyCustomCommonIdlingResource)
-    }
+            waitUntilAtLeastOneExists(hasTestTag("ValuePresenter") and hasText("0"))
+        }
 }
